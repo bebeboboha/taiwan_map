7fcde74f0fe64972e06dee1a7dc8b36f5fdcf38e
Peggy Chan <bebebobohaha@gmail.com>
2022-07-06 11:15:01 +0800

    [feat] 點選新增企業

--- src/taiwan_map.js
+++ src/taiwan_map.js
@@ -1,8 +1,8 @@
 const TaiwanMap = new Vue({
     el: '#taiwan_map',
     data: {
-      h1: '縣市中文',
-      h2: '縣市英文'
+      h1: '',
+      companies: []
     },
     methods: {
       async getTaiwanMap() {
@@ -28,14 +28,14 @@ const TaiwanMap = new Vue({
             .mercator()
             .center([121,24])
             .scale(mercatorScale)
-            .translate([width/2, height/2.5])
+            .translate([width, height/2.5])
         );
         
         // 讓d3抓svg，並寫入寬高
         var svg = await d3.select('#svg')
             .attr('width', width)
             .attr('height', height)
-            .attr('viewBox', `0 0 ${width} ${height}`);
+            .attr('viewBox', `0 0 ${width*2} ${height}`);
         
         // 讓d3抓GeoJSON檔，並寫入path的路徑
         var url = 'https://raw.githubusercontent.com/bebeboboha/taiwan_map/main/src/taiwan_area.geojson';
@@ -52,8 +52,8 @@ const TaiwanMap = new Vue({
               id: (d) => 'city' + d.properties.COUNTYCODE
             })
             .on('click', d => {
-              this.h1 = d.properties.COUNTYNAME; // 換中文名
-              this.h2 = d.properties.COUNTYENG; // 換英文名
+              this.h1 = d.properties.COUNTYENG; // 換英文名
+              this.companies = d.properties.COMPANIES; // 換企業名
               // 有 .active 存在，就移除 .active
               if(document.querySelector('.active')) {
                 document.querySelector('.active').classList.remove('active');
