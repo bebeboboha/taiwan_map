eb99ee19571a270891834cd1d64f6213aa12cff9
Peggy Chan <bebebobohaha@gmail.com>
2023-07-04 15:33:37 +0800

    [feat] add taiwan map

--- src/taiwan_map.js
+++ src/taiwan_map.js
@@ -44,7 +44,7 @@ const TaiwanMap = new Vue({
             .attr('viewBox', `0 0 ${width*2} ${height}`);
         
         // 讓d3抓GeoJSON檔，並寫入path的路徑
-        var url = 'https://raw.githubusercontent.com/bebeboboha/taiwan_map/main/src/taiwan_area.geojson';
+        var url = 'https://raw.githubusercontent.com/bebeboboha/taiwan_map/main/src/taiwan.geojson';
         await d3.json(url, (error, geometry) => {
           if (error) throw error;
   
